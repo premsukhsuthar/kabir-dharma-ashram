c025f272650670eb2e670db4efe9762482712c67
premsukhsuthar <premsukhsuthar84@gmail.com>
2026-02-12 21:35:56 +0530

    Fix invalid INSTALLED_APPS syntax

--- build.sh
+++ build.sh
@@ -2,9 +2,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-
-python manage.py collectstatic --noinput
-python manage.py makemigrations
 python manage.py migrate --noinput
-python manage.py compilemessages
-
+python manage.py collectstatic --noinput
