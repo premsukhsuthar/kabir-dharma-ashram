5cd777cbab57b6bd8bd867cd0a48e84d20689bcc
premsukhsuthar <premsukhsuthar84@gmail.com>
2026-02-12 21:18:42 +0530

    Run migrations on deploy to create database tables

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py compilemessages
+
